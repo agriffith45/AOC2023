5da342e1e345ac385cc600004c3781f5a1c1901c
Andy Griffith <andy.d.griffith@gmail.com>
2024-12-03 15:48:51 -0700

    second answer to day 1

--- 1/main.go
+++ 1/main.go
@@ -25,7 +25,19 @@ func main() {
 	}
 	evens, odds := splitIntoOddsAndEvens(lines)
 	answer := doTheMath(evens, odds)
-	fmt.Println("the answer is: ", answer)
+	fmt.Println("the answer to part 1 is: ", answer)
+	//get a unique list of all values in the left list
+	valueMap := makeMapToTrackSimilarityScore(evens)
+	similarityScoreMap := findOutandRecordSimilarityScore(valueMap, odds)
+	answerPart2 := 0
+	for key := range similarityScoreMap {
+		//		fmt.Println("key ", key, " hits ", similarityScoreMap[key])
+		if similarityScoreMap[key] != 0 {
+			tempAnswer := key * similarityScoreMap[key]
+			answerPart2 = answerPart2 + tempAnswer
+		}
+	}
+	fmt.Println("Answer to Part 2 is ", answerPart2)
 
 }
 
@@ -41,6 +53,28 @@ func splitIntoOddsAndEvens(arg1 []string) (evens []int, odds []int) {
 		}
 	}
 	return evens, odds
+}
+
+func findOutandRecordSimilarityScore(similarityScoreMap map[int]int, odds []int) map[int]int {
+	var numberofHits int
+	for key := range similarityScoreMap {
+		numberofHits = 0
+		for i := 0; i < len(odds); i++ {
+			if odds[i] == key {
+				numberofHits = numberofHits + 1
+			}
+			similarityScoreMap[key] = numberofHits
+		}
+	}
+	return similarityScoreMap
+}
+
+func makeMapToTrackSimilarityScore(evens []int) map[int]int {
+	similarityScoreMap := make(map[int]int)
+	for i := 0; i < len(evens); i++ {
+		similarityScoreMap[evens[i]] = 0
+	}
+	return similarityScoreMap
 }
 
 func doTheMath(evens []int, odds []int) int {
